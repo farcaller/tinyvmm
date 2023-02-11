a46afe6e149dd3f03152dd2acf328c49209177bf
Vladimir Pouzanov <farcaller@gmail.com>
2023-02-11 13:24:42 +0000

    Add more useful debug logging for the reconciler

--- tinyvmm/src/unitserver/virtualmachines.rs
+++ tinyvmm/src/unitserver/virtualmachines.rs
@@ -10,13 +10,18 @@ pub async fn reconcile(runtime_dir: &str) -> eyre::Result<()> {
 
     let vms = VirtualMachine::list(runtime_dir)?;
 
+    debug!("got {} vms to reconcile", vms.len());
+
     // generate the units for existing vms and check the diffs
     // if there are any diffs, commit, daemon-reload and start them
     for vm in vms {
         let name = &vm.metadata.name;
         let bridge_name = &vm.spec.bridge;
 
+        debug!("reconciling vm {name}");
+
         let has_diffs = systemd::has_diffs(name, bridge_name, runtime_dir, self_exe).await;
+        debug!("vm {name} diffs: {has_diffs:?}");
         match has_diffs {
             Err(e) => {
                 info!("failed to check for diffs for {}: {}", name, e);
